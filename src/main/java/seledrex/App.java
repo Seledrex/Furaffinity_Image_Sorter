b76df218d1665d53e10aa802b01d76b153b72bb7
Eric Auster <Eric Auster>
2017-07-15 23:26:52 -0500

    Initial commit

---
+++ src/main/java/seledrex/App.java
@@ -0,0 +1,188 @@
+package seledrex;
+
+//======================================================================================================================
+// Imports
+//======================================================================================================================
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+
+//======================================================================================================================
+// App
+//======================================================================================================================
+
+/**
+ * This class implements the GUI for the Furaffinity Image Sorter.
+ *
+ * The application window is created in the theme of the user's operating
+ * system, and then the window is show in the center of the user's
+ * primary screen. A log is created in the center below a panel that holds
+ * all the buttons.
+ *
+ * @author Eric Auster
+ */
+public class App extends JPanel implements ActionListener
+{
+    //==================================================================================================================
+    // Properties
+    //==================================================================================================================
+
+    private JButton addFolderButton, clearInputButton, setOutputButton, sortButton; // Buttons
+    private JTextArea log; // Log
+    private JFileChooser fc; // File chooser
+    private ImageSorting sorter; // Image sorter
+
+    //==================================================================================================================
+    // Constructor
+    //==================================================================================================================
+
+    private App()
+    {
+        super(new BorderLayout());
+
+        // Create the log object and make it scrollable
+        log = new JTextArea(20,100);
+        log.setMargin(new Insets(5,5,5,5));
+        log.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(log);
+
+        // Create the file chooser
+        fc = new JFileChooser();
+
+        // Create the image sorter
+        sorter = new ImageSorting();
+
+        // Set the image sorter so that it will only show directories
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        // Create the 'Add input folder' button
+        addFolderButton = new JButton("Add input folder");
+        addFolderButton.addActionListener(this);
+
+        // Create the 'Sort images' button
+        sortButton = new JButton("Sort images");
+        sortButton.addActionListener(this);
+
+        // Create the 'Set output folder' button
+        setOutputButton = new JButton("Set output folder");
+        setOutputButton.addActionListener(this);
+
+        // Create the 'Remove input folder(s)' button
+        clearInputButton = new JButton("Remove input folder(s)");
+        clearInputButton.addActionListener(this);
+
+        // Create a new panel to hold all the buttons
+        JPanel buttonPanel = new JPanel(); //use FlowLayout
+        buttonPanel.add(addFolderButton);
+        buttonPanel.add(clearInputButton);
+        buttonPanel.add(setOutputButton);
+        buttonPanel.add(sortButton);
+
+        // Add the button panel and log to the main panel
+        add(buttonPanel, BorderLayout.PAGE_START);
+        add(logScrollPane, BorderLayout.CENTER);
+    }
+
+    //==================================================================================================================
+    // Methods
+    //==================================================================================================================
+
+    /**
+     * Picks up events created when a button is pressed. Based on
+     * which button is pressed, a different action will be performed.
+     *
+     * @param e  event created
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        // Handles the 'Add input folder' button
+        if (e.getSource() == addFolderButton)
+        {
+            int returnVal = fc.showOpenDialog(App.this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION)
+            {
+                File file = fc.getSelectedFile();
+                log.append("Added folder to be sorted: " + file.getAbsolutePath() + "\n");
+                sorter.addInputFolder(file.getAbsolutePath());
+            }
+
+            log.setCaretPosition(log.getDocument().getLength());
+        }
+        // Handles the 'Set output folder' button
+        else if (e.getSource() == setOutputButton)
+        {
+            int returnVal = fc.showOpenDialog(App.this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION)
+            {
+                File file = fc.getSelectedFile();
+                log.append("Set output folder: " + file.getAbsolutePath() + "\n");
+                sorter.setOutputFolder(file);
+            }
+
+            log.setCaretPosition(log.getDocument().getLength());
+        }
+        // Handles the 'Sort images' button
+        else if (e.getSource() == sortButton)
+        {
+            log.append("Sorting images..." + "\n");
+            sorter.sortImages(log);
+            log.setCaretPosition(log.getDocument().getLength());
+        }
+        // Handles the 'Remove input folder(s)' button
+        else if (e.getSource() == clearInputButton)
+        {
+            sorter.clearInputFolders();
+            log.append("Removed input folder(s)");
+        }
+    }
+
+    /**
+     * Builds the GUI and then shows the window.
+     */
+    private static void createAndShowGUI()
+    {
+        // Creates the JFrame
+        JFrame frame = new JFrame("Furaffinity Image Sorter");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(new App());
+
+        // Set size and center window
+        frame.setSize(1280, 720);
+        frame.setLocationRelativeTo(null);
+
+        // Create and show window
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    //==================================================================================================================
+    // Main
+    //==================================================================================================================
+
+    /**
+     * Main entry point for the program. Sets the look and feel of
+     * the GUI to whichever operating system the user is using.
+     *
+     * @param args  command line arguments
+     */
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                createAndShowGUI();
+            }
+        });
+    }
+}
---
+++ src/main/java/seledrex/ImageSorting.java
@@ -0,0 +1,234 @@
+package seledrex;
+
+//======================================================================================================================
+// Imports
+//======================================================================================================================
+
+import javafx.util.Pair;
+import org.apache.commons.io.FileUtils;
+import javax.swing.*;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//======================================================================================================================
+// Image Sorting
+//======================================================================================================================
+
+/**
+ * Implements the backend of the GUI, where all the image sorting
+ * actually occurs. All the files inside the specified directories
+ * are read into an array. Then the filenames and paths are read
+ * into the program. Afterwards, a regular expression is used on
+ * each of the filenames to check if it can be sorted. If a match is
+ * found, then the artist name is extracted, and the artist is added
+ * to a hash map that keeps track of all the artists found. Files
+ * are the associated with each artist, where each artist has a list
+ * of files. An output.txt file is created to show all the files
+ * sorted, and then the images are copied to their respective
+ * directories. If an artist does not have a directory made for him
+ * or her, then a new one is created. If the copied file already exists,
+ * then it will not be copied.
+ *
+ * @author Eric Auster
+ */
+class ImageSorting
+{
+    //==================================================================================================================
+    // Properties
+    //==================================================================================================================
+
+    private List<Pair<String, String>> files; // Holds a list of all the files to sort
+    private Map<String, List<Pair<String, String>>> gallery; // Maps artists to their respective artwork
+    private List<File[]> inputFolders; // Stores the input folders supplied by user
+    private File outputFolder; // Stores the output folder supplied by user
+
+    //==================================================================================================================
+    // Constructor
+    //==================================================================================================================
+
+    /**
+     * Initializes the data structures and variables for the
+     * image sorter.
+     */
+    ImageSorting()
+    {
+        files = new ArrayList<Pair<String, String>>();
+        gallery = new HashMap<String, List<Pair<String, String>>>();
+        inputFolders = new ArrayList<File[]>();
+        outputFolder = null;
+    }
+
+    //==================================================================================================================
+    // Methods
+    //==================================================================================================================
+
+    /**
+     * Adds an input folder to the input folder list. An array of
+     * files is created immediately.
+     *
+     * @param path  path to input folder
+     */
+    void addInputFolder(String path) { inputFolders.add(new File(path).listFiles()); }
+
+    /**
+     * Clears all the input folders currently stored.
+     */
+    void clearInputFolders() { inputFolders.clear(); }
+
+    /**
+     * Sets the output folder that all files will be saved to.
+     *
+     * @param outputFolder  path to output folder
+     */
+    void setOutputFolder(File outputFolder) { this.outputFolder = outputFolder; }
+
+    //==================================================================================================================
+    // Sorting
+    //==================================================================================================================
+
+    /**
+     * Where all the fun really happens! Each individual file is parsed
+     * using the regular expression, and then are properly sorted and
+     * copied to their proper locations.
+     *
+     * @param log  log to print to
+     */
+    void sortImages(JTextArea log)
+    {
+        // Check if an input folder was given
+        if (inputFolders.isEmpty()) {
+            log.append("Input folder(s) not added\n");
+            return;
+        }
+
+        // Check if an output folder was given
+        if (outputFolder == null) {
+            log.append("Output folder not set");
+            return;
+        }
+
+        // Loops through each of the input folders
+        for (File[] inputFolder : inputFolders)
+        {
+            if (inputFolder != null)
+            {
+                // Add each file inside of the input folder
+                for (File file : inputFolder) {
+                    this.files.add( new Pair<String, String>(file.getName(), file.getAbsolutePath()));
+                }
+            }
+
+            // Create regular expression
+            Pattern pattern = Pattern.compile("(\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d.)([^_]*)(_)(.*)");
+
+            // Loop through each of the files
+            for (Pair<String, String> file : this.files)
+            {
+                // Use regular expression
+                Matcher m = pattern.matcher(file.getKey());
+
+                // Pattern is found
+                if (m.find())
+                {
+                    // Extract artist
+                    String artist = m.group(2);
+
+                    // Check if the artist is in the gallery yet
+                    if (!gallery.containsKey(artist)) {
+                        gallery.put(artist, new ArrayList<Pair<String, String>>());
+                    }
+
+                    // Add the file to artist
+                    gallery.get(artist).add(file);
+                }
+                // Pattern not found
+                else
+                {
+                    // Check if unsorted has been created
+                    if (!gallery.containsKey("unsorted")) {
+                        gallery.put("unsorted", new ArrayList<Pair<String, String>>());
+                    }
+
+                    // Add the file to unsorted
+                    gallery.get("unsorted").add(file);
+                }
+            }
+        }
+
+        // Grab all the artists found
+        Set<String> artists = gallery.keySet();
+        PrintWriter writer = null;
+
+        try {
+            // Create a print writer
+            writer = new PrintWriter(outputFolder.getAbsoluteFile() + "\\output.txt");
+
+            // Loop through each artist
+            for (String artist : artists)
+            {
+                // Get the artwork for the artist
+                List<Pair<String, String>> artwork = gallery.get(artist);
+
+                // Print the artist to file
+                writer.format(artist + ":\n");
+
+                int num = 1;
+
+                // Loop through all the files belonging to the artist
+                for (Pair<String, String> file : artwork)
+                {
+                    // Write the number and then filename to file
+                    writer.format("\t" + num + ". " + file.getKey() + "\n");
+                    num++;
+                }
+                writer.format("\n");
+            }
+
+        // Catch exceptions
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) writer.close();
+        }
+
+        // Loop through all the artists
+        for (String artist : artists)
+        {
+            // Get the artwork for the artist
+            List<Pair<String, String>> artwork = gallery.get(artist);
+
+            // Open the directory for the artist
+            File artistDir = new File(outputFolder.getAbsoluteFile() + "\\" + artist);
+
+            // If the directory does not exist, create it
+            if (!artistDir.exists())
+            {
+                if (artistDir.mkdir()) {
+                    log.append("Made new directory: " + artistDir.getAbsolutePath() + "\n");
+                }
+            }
+
+            // Loop through all the files belonging to the artist
+            for (Pair<String, String> filename : artwork)
+            {
+                // Open file inside artist directory
+                File check = new File(artistDir.getAbsolutePath() + "\\" + filename.getKey());
+
+                // If the file does not exist, copy it
+                if (!check.exists())
+                {
+                    try {
+                        FileUtils.copyFile(new File(filename.getValue()),
+                                new File(artistDir.getAbsolutePath() + "\\" + filename.getKey()));
+                        log.append("Copied: " + filename.getKey() + "\n");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
